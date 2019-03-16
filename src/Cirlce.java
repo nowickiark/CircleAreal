d6a57b682acf82ca7d3a21fbc864ff44e0b7af76
Arek Nowicki <nowickiark@outlook.com>
2019-03-16 10:26:23 +0100

    Calcultes area of a circle

---
+++ src/Cirlce.java
@@ -0,0 +1,10 @@
+public class Cirlce {
+
+    private double diameter;
+    private int noLoops = 100000000;
+    private double [] CounterArray = {0,0};
+
+   /* public Cirlce(double diameter, double )*/
+
+
+}
